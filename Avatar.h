24af1174126e2c9ab0db5488bd7654a1a0e3fccf
Dai Luong <luong@DESKTOP-INLVFFC.localdomain>
2023-06-02 16:08:00 +0930

    Kenny

--- Avatar.h
+++ Avatar.h
@@ -1,5 +1,6 @@
 #ifndef AVATAR_H
 #define AVATAR_H
+#include <iostream>
 #include <tuple>
 #include "GridUnit.h"
 
@@ -10,6 +11,8 @@ class Avatar: public GridUnit {
         void shift(int dx, int dy);
 
     protected:
+        // std::tuple<int, int> coor;
+        GridUnit grid;
 
 };
 
@@ -19,9 +22,15 @@ Avatar::Avatar(int x, int y): GridUnit(x, y, 'A') {
 }
 
 void Avatar::shift(int dx, int dy) {
+    // cout << "Me" << endl;
+    // int x = std::get<0>(grid.getCoordinates()) + dx;
+    // int y = std::get<1>(grid.getCoordinates()) + dy;
+    // grid.setCoordinates(x, y);
+    // std::get<0>(coor) = x;
+    // std::get<1>(coor) = y;
     std::tuple<int, int> coor;
     std::get<0>(coor) = std::get<0>(coor) + dx;
-    std::get<1>(coor) = std::get<1>(coor) + dx;
+    std::get<1>(coor) = std::get<1>(coor) + dy;
 }
 
 #endif
